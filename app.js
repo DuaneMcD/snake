ebedae616bd4aff440bee80086e1b39b622aa4ac
Duane McDonald <mcdonald.duane13@gmail.com>
2021-03-20 08:17:52 -0400

    wireframed design

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // snakey snake time
+// Deliverables;
+
+// Snake game ends when:
+// Snake touches itself
+// Snake touches the outside border
+// When the game ends, the gameplay should stop and the user should be notified that the game is over
+
+// The snake should grow one length whenever it eats the apple
+// The apple should randomly place itself on the board after snake consumes the apple
+
+// The snake should be controlled by the arrow keys on the keyboard
+// The game will  show a score of how many apples have been eaten
