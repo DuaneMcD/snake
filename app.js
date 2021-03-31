37367101ce3d3ee4d7a6899533adf91f94c660ed
Duane McDonald <mcdonald.duane13@gmail.com>
2021-03-31 12:32:18 -0400

    collisions working

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const canvas = document.querySelector('.field');
 const ctx = canvas.getContext('2d');
 let interval = "";
-let snake= {
+let snake = {
     X: 50,
     Y: 50,
+    width:5,
+    height:15,
+    body = {
+        
+    }
 }
 let apple = {
     X: 0,
     Y: 0,
+    width:10,
+    height:8,
 }
 let score = 0;
 
@@ -34,8 +41,50 @@ function input(arrow) {
 
 function moveSnake(arrow) {
     snakeDirection(arrow);
+    collision();
     killSnake();
-    eatApple();
+}
+
+function drawGame() {
+    ctx.fillStyle = '#f7d13a';
+    ctx.fillRect(0,0,canvas.clientWidth,canvas.height); 
+    ctx.fillStyle = 'green';
+    ctx.fillRect(snake.X,snake.Y,snake.width,snake.height); 
+    ctx.fillStyle = 'red';
+    ctx.fillRect(apple.X,apple.Y,apple.width,apple.height);
+}
+
+function snakeDirection(arrow) {
+    switch(arrow) {
+    case "left":
+        return snake.X -= 2;
+    case "right":
+        return snake.X += 2;
+    case "up":
+        return snake.Y -= 2;    
+    case "down":
+        return snake.Y += 2;    
+    }
+}
+
+function collision() {
+    if (snake.X + snake.width > apple.X  &&
+        snake.X < apple.X + apple.width &&
+        snake.Y + snake.height > apple.Y &&
+        snake.Y < apple.Y + apple.height 
+        ){ eatApple();
+    }
+}
+
+function eatApple() {
+    score ++;
+    document.querySelector('.scoreDisplay').textContent = addLeadingZeros(score);
+    randomApple();
+    console.log("yummy apple")
+}
+
+function addLeadingZeros (number) {
+    return number < 10 ? '00' + number : (number < 100 ? '0' + number : number);
 }
 
 function killSnake() {
@@ -53,49 +102,10 @@ function deadSnake() {
     snake.Y = 50;
 }
 
-function snakeDirection(arrow) {
-    switch(arrow) {
-    case "left":
-        console.log(snake.X, snake.Y);
-        return snake.X -= 2;
-    case "right":
-        console.log(snake.X, snake.Y);
-        return snake.X += 2;
-    case "up":
-        console.log(snake.X, snake.Y);
-        return snake.Y -= 2;    
-    case "down":
-        console.log(snake.X, snake.Y);
-        return snake.Y += 2;    
-    }
-}
-
 function randomApple() {
     apple.X = Math.floor(Math.random() * canvas.width);
     apple.Y = Math.floor(Math.random() * canvas.height);
 }
-
-function drawGame() {
-    ctx.fillStyle = '#f7d13a';
-    ctx.fillRect(0,0,canvas.clientWidth,canvas.height); 
-    ctx.fillStyle = 'green';
-    ctx.fillRect(snake.X,snake.Y,5,15); 
-    ctx.fillStyle = 'red';
-    ctx.fillRect(apple.X,apple.Y,5,5);
-}
-
-function eatApple() {
-    if ((snake.X >= apple || snake.X <= apple + apple.width) && (snake.Y >= apple || snake.Y <= apple + apple.height)) {
-    score ++;
-    document.querySelector('.scoreDisplay').textContent = addLeadingZeros(score);
-    randomApple();
-    console.log("yummy apple")
-    }
-}
-
-const addLeadingZeros = (number) => {
-    return number < 10 ? '00' + number : (number < 100 ? '0' + number : number);
-};
 
 document.addEventListener('DOMContentLoaded', randomApple);
 document.addEventListener('DOMContentLoaded', drawGame);
